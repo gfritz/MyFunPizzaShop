2afe2eab5e683815981239db2630385fae6677f7
gfritz <gfritz@cardlytics.com>
2023-07-18 08:40:43 +0000

    add automation baseline to read our single feature

--- test/Automation/Program.fs
+++ test/Automation/Program.fs
@@ -1,2 +1,26 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿module FunPizzaShop.Automation.Program
+
+open System.Reflection
+open TickSpec
+
+[<EntryPointAttribute>]
+let main _ =
+    try
+        try
+            do
+                let assembly = Assembly.GetExecutingAssembly()
+                let definitions = StepDefinitions(assembly)
+
+                // get PizzaMenu.feature from embedded resource
+                ["PizzaMenu"]
+                |> Seq.iter (fun source ->
+                    let s = assembly.GetManifestResourceStream("Automation." + source + ".feature")
+                    definitions.Execute(source, s))
+            0
+        with e ->
+            printfn "%s" (e.ToString())
+            // console exit code
+            -1
+    finally
+        ()
+        // host.StopAsync().Wait()

9c4b8d100b48c686385e0f15abf1fea1b32181a5
gfritz <gfritz@cardlytics.com>
2023-07-20 11:07:59 +0000

    spelling and commentary

--- src/Command/API.fs
+++ src/Command/API.fs
@@ -1,4 +1,23 @@
+/// The API for whoever wants to communicate with our Actors
 module FunPizzaShop.Command.API
+
+// NOTE: our customized error handling happens in here Error { ... }
+// module User =
+// subscribe:
+// If no message ever happens, then it waits indefinitely.
+// For each subscription, Common.fs creates a new Actor, assigns a correlationId per command
+// verify:
+// ...
+//
+// Is all this trouble worth it? Onur says for him yes.
+// Once you setup your project with the akka foundation, it is easy to integrate additional actors.
+// However, the boilerplate gives weight to an argument to use Marten or use CRUD for simple cases.
+// Of course, simple cases grow into not simple and it may be HARDER to change at that point while this
+// akka actor setup is rather extensible.
+//
+// TODO: would have been better to include Order
+//  when 2 actors need to communicate with eachother, you really want a saga
+//  but that is not done here; possibly to show the ease of adding additional
 
 open Command
 open Common
@@ -68,7 +87,7 @@ module User =
                 Log.Debug("Inside Verify {@userId} {@verCode}", userId, verCode)
 
                 let subscribA =
-                    createSubs (userId.Value) (User.VefifyLogin verCode) (function
+                    createSubs (userId.Value) (User.VerifyLogin verCode) (function
                         | User.VerificationFailed
                         | User.VerificationSucceeded _ -> true
                         | _ -> false)
@@ -102,9 +121,9 @@ let api (env: _) (clock: IClock) =
     let userSubs = createCommandSubscription domainApi domainApi.UserFactory
 
     { new IAPI with
-        member this.Login: Login = 
+        member this.Login: Login =
             User.login userSubs
-        member this.Verify: Verify = 
+        member this.Verify: Verify =
             User.verify userSubs
         member _.ActorApi = actorApi
     }
